7e4cfb14b3492e6f7136428774192977af1cb7b3
Greg Pelkola <141973953+gregpelkola@users.noreply.github.com>
2024-02-12 21:15:59 -0600

    set imports for savedbooks

--- Develop/client/src/pages/SavedBooks.jsx
+++ Develop/client/src/pages/SavedBooks.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import React from 'react';
 import {
   Container,
   Card,
@@ -10,9 +10,14 @@ import {
 import { getMe, deleteBook } from '../utils/API';
 import Auth from '../utils/auth';
 import { removeBookId } from '../utils/localStorage';
+import {useQuery, useMutation} from '@apollo/client';
+import {GET_ME} from '../utils/queries';
 
 const SavedBooks = () => {
-  const [userData, setUserData] = useState({});
+  const { data } = useQuery(GET_ME); 
+  const [deleteBook] = useMutation(REMOVE_BOOK);
+
+  const userData = data?.me || {};
 
   // use this to determine if `useEffect()` hook needs to run again
   const userDataLength = Object.keys(userData).length;
